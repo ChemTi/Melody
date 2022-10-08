243d8de57fb258d83aea53617a593a6dab52f5ef
Timur_Chemazokov <79016313+ChemTi@users.noreply.github.com>
2022-10-08 13:35:12 +0300

    клик по кнопке <<Смотреть квратиры на этажах>>

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
 	var floorPath = $('.home-image path'); // каждый отдельный этаж в SVG
 	var counterUp = $('.counter-up'); // кнопка увеличения этажа
 	var counterDown = $('.counter-down'); // кнопка уменьшения этажа
+	var modal = $('.modal'); // модальное окно
+	var modalCloseButton = $('.modal-close-button'); // кнопка закрытия модального окна
+	var buttonViewFlats = $('.button-view-flats'); // кнопка просмотра этажей
 
 	// наведение мыши на этаж
 	floorPath.on('mouseover', function () {
@@ -10,6 +13,10 @@ $(document).ready(function () {
 		currentFloor = $(this).attr('data-floor'); // получаем значение текущего этажа
 		$('.counter').text(currentFloor); // записываем значие этажа в счетчик
 	});
+
+	floorPath.on('click', toggleModal); // клик на этаж - вызвать модальное окно
+	modalCloseButton.on('click', toggleModal); // клик закрыть модальное окно
+	buttonViewFlats.on('click', toggleModal); // клик по кнопке смотреть квартиры на этаже
 
 	counterUp.on('click', function () { // отслеживаем клик по кнопке вверх
 		if (currentFloor < 18) { // проверяем значение этажа не больше 18
@@ -30,5 +37,9 @@ $(document).ready(function () {
 			$(`[data-floor=${usFloors}]`).toggleClass('current-floor'); // подсвечиваем текущий этаж
 		} 
 	});		
+
+	function toggleModal() { // ф-я открыть/закрыть окно
+		modal.toggleClass('is-open');
+	}
 });
 
